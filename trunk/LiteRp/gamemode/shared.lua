4b075b0617a248a2e4f714dd6432a32ae7c22ceb
slay3r36 <slay3r36@9a43baf6-010a-eb20-d6cc-cd9462e2e08f>
2010-06-19 02:04:34 +0000

    [Change] Derive the gamemode from sandbox. this will simplify couples of things

--- trunk/LiteRp/gamemode/shared.lua
+++ trunk/LiteRp/gamemode/shared.lua
@@ -3,7 +3,7 @@ GM.Name		= "LiteRp"
 GM.Author	= "Slay3r36"
 GM.Email	= "lag.gmod@gmail.com"
 GM.Website  = "lag-server.tk"
---DeriveGamemode( "base" )-- Do you realy think i'll write it from scratch
+DeriveGamemode( "sandbox" )-- Do you realy think i'll write it from scratch
  
 team.SetUp( 1, "Guest", Color( 255, 0, 0, 150 ) )
 team.SetUp( 2, "Flic", Color( 0, 0, 255, 150 ) )
